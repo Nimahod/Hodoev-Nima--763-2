41862170343b209763f6fda9d71710bbbd0d23a8
Nimahod <hodoevnima@gmail.com>
2024-06-26 05:09:04 +0800

    Add files via upload

---
+++ 5/prog.java
@@ -0,0 +1,69 @@
+package practic;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+class Account {
+    private String accountNumber;
+    private double balance;
+    private ArrayList<Transaction> transactions;
+
+    public Account(String accountNumber) {
+        this.accountNumber = accountNumber;
+        this.balance = 0;
+        this.transactions = new ArrayList<>();
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+        transactions.add(new Transaction("поступление", amount));
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            transactions.add(new Transaction("снятие", amount));
+        } else {
+            System.out.println("Недостаточно средств на счете.");
+        }
+    }
+
+    public void printTransactions() {
+        for (Transaction t : transactions) {
+            System.out.println(t);
+        }
+    }
+
+    private class Transaction {
+        private String type;
+        private double amount;
+        private Date date;
+
+        public Transaction(String type, double amount) {
+            this.type = type;
+            this.amount = amount;
+            this.date = new Date(); // текущая дата и время
+        }
+
+        @Override
+        public String toString() {
+            return "Тип операции: " + type + ", Сумма: " + amount + ", Дата: " + date.toString();
+        }
+    }
+}
+
+public class prog {
+    public static void main(String[] args) {
+        // Создание нового счета
+        Account myAccount = new Account("123456789");
+
+        // Проведение операций со счетом
+        myAccount.deposit(1000); // Поступление
+        myAccount.withdraw(500); // Снятие
+        myAccount.deposit(1500); // Поступление
+        myAccount.withdraw(700); // Снятие
+
+        // Вывод информации обо всех операциях
+        myAccount.printTransactions();
+    }
+}
