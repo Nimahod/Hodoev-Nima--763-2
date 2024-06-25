790731197c282b9ad61b541ea905cef380d630d8
Nimahod <hodoevnima@gmail.com>
2024-06-26 05:46:02 +0800

    Add files via upload

---
+++ 7/prog.java
@@ -0,0 +1,19 @@
+package practic;
+
+import java.util.function.BiFunction;
+
+public class prog {
+    public static void main(String[] args) {
+        
+        BiFunction<Double, Double, Double> hypotenuse = (a, b) -> Math.sqrt(a * a + b * b);
+
+        BiFunction<Double, Double, Double> radius = (a, b) -> (a + b - hypotenuse.apply(a, b)) / 2;
+
+        double a = 8.0;
+        double b = 6.0;
+        double r = radius.apply(a, b); 
+
+        System.out.println("Радиус вписанной окружности: " + r);
+    }
+}
+
